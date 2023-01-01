873e7a316899bce0fba37da0a620c3d8d472be37
bylkode <hafidsaadibk@gmail.com>
2023-01-01 01:51:50 -0500

    create initial structure

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+
+// ******************************************************
+// ************* Get the Preferred Network **************
+// ******************************************************   
+
+    function getNet() {
+                let net
+          if (document.getElementById("tn").checked) net = "wss://s.altnet.rippletest.net:51233"
+          if (document.getElementById("dn").checked) net = "wss://s.devnet.rippletest.net:51233"
+          return net
+        } // End of getNet()
